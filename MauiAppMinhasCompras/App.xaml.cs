5e5a39cd3521f069437b8db37e322d7a141d883f
viniciusam0rim <viniciusamorim0104@gmail.com>
2026-08-14 22:56:27 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+    }
+}
---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+
+        [ PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+
+        public double Preço { get; set; }
+
+    }
+}
